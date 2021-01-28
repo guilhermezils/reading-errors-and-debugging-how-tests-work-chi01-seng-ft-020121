f4f3e17e1befda23919f32f8f419774408de5b1c
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-28 23:48:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,23 @@
-# Add your variables here
+# Add your variables here.
+require 'pry'
+
+def calculators
+
+first_number = 111
+
+
+
+second_number = 222
+
+
+sum == first_number+second_number
+
+
+difference == first_number - second_number
+
+
+product == first_number*second_number
+
+
+quotient == first_number/second_number
+end
